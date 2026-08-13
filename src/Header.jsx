bfdbfaa6ed3e4bea20eb05ccb1825640ad6ea8fa
asm-test <asm-test@info>
2026-08-13 03:28:38 -1100

    complete header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react"
 import dark from "./assets/dark.svg"
 export const Header = () => {
+    const [isClickHambuger, setIsClickHambuger] = useState(false);
+    const [isClickBar1, setIsClickBar1] = useState(false)
+    const [isClickBar2, setIsClickBar2] = useState(false)
+    const [isClickBar3, setIsClickBar3] = useState(false)
+
+    const handleClickHambuger = ()=>{
+        setIsClickHambuger(!isClickHambuger)
+        setIsClickBar1(!isClickBar1)
+        setIsClickBar2(!isClickBar2)
+        setIsClickBar3(!isClickBar3)
+    }
+
     return(
         <div>
         <header className="header">
             <div className="logo-name">ASM Global Ventures</div>
-            <nav className="nav-links">
+            <nav className={!isClickHambuger ? "nav-links" : "show"}>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Services</a></li>
                 <li><a href="#">About</a></li>
@@ -14,10 +27,10 @@ export const Header = () => {
                 <div className="setting">
                     <img style={{width: "20px"}} src={dark} alt="" />
                 </div>
-                <div className="hambuger">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
+                <div onClick={handleClickHambuger} className="hambuger">
+                    <span className={!isClickBar1 ? "bar" : "bar-style1"}></span>
+                    <span className={!isClickBar2 ? "bar" : "bar-style2"}></span>
+                    <span className={!isClickBar3 ? "bar" : "bar-style3"}></span>
                     
                 </div>
             </div>
