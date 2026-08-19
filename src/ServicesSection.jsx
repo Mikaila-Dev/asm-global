cb06b15b8ef7698d9897fa129cc86ec1a8c69d49
asm-test <asm-test@info>
2026-08-18 22:07:04 -1100

    adding some cards in services section

--- src/ServicesSection.jsx
+++ src/ServicesSection.jsx
@@ -2,30 +2,76 @@
 
 export const ServicesSection = () => {
     return(
-        <div className="services-section">
-         <h3 style={{
-            color: "white"
-         }}>What We Offer</h3>
-            <div className="p">Quality products and reliable services, all in one convenient place.</div>
-
-        <div className="phone-accessories" style={{
-            color: "white"
-        }}>
-            <div>
-            <h3>Phone Accessories</h3>
-            <p>
+    <div className="services-section">
+         <h3 style={{color: "white"}} className="sercices-heading">What We Offer</h3>
+    <div className="p">Quality products and reliable services, all in one convenient place.</div>
+    <div style={{gap: "20px"}} className="main-services">
+    <div className="phone-accessories" style={{color: "white"}}>
+    <h3>Phone Accessories</h3>
+    <p>
     Find reliable phone accessories made to keep your devices
     powered, protected, and ready to go.
     </p>
     <div className="service-tags">
-  <span>Quality Cables</span>
-  <span>Fast Chargers</span>
-  <span>Earphones</span>
-  <span>Phone Cases</span>
-  <span>Screen Protectors</span>
+        <span>Quality Cables</span>
+        <span>Fast Chargers</span>
+        <span>Earphones</span>
+        <span>Phone Cases</span>
+        <span>Screen Protectors</span>
+  </div>
 </div>
-            </div>
-        </div>
-        </div>
+
+
+<div className="airtime-and-data-section" style={{color: "white"}}>
+<h3>Data & Airtime</h3>
+<p>
+  Stay connected with fast, reliable data and airtime services,
+  available whenever you need them.
+</p>
+    <div className="service-tags">
+    <span>Data Bundles</span>
+    <span>Airtime</span>
+    <span>MTN</span>
+    <span>Airtel</span>
+    <span>Glo</span>
+    <span>9mobile</span>
+    </div>
+  </div>
+
+<div className="movies-container" style={{color: "white"}}>
+<h3>Movies</h3>
+<p>
+  Enjoy your favorite Kannywood and Bollywood movies,
+  available for easy and convenient access.
+</p>
+<div className="service-tags">
+  <span>Kannywood</span>
+  <span>Bollywood</span>
+  <span>Latest Movies</span>
+  <span>Classic Movies</span>
+ </div>
+</div>
+
+
+<div className="drinks" style={{color: "white"}}>
+    <h3>Cold Drinks</h3>
+
+<p>
+  Refresh yourself with a variety of cold and refreshing drinks,
+  perfect for every moment and occasion.
+</p>
+
+<div class="service-tags">
+  <span>Soft Drinks</span>
+  <span>Energy Drinks</span>
+  <span>Water</span>
+  <span>Juices</span>
+  <span>Cold & Fresh</span>
+</div>
+</div>
+
+
+ </div>
+</div>
     )
 }
